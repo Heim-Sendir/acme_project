b7867311a92f2d17a04ad735fd6d668f698869c5
Heim_Sendir <alex.noa29@gamil.com>
2024-12-05 17:10:20 +0500

    start project

---
+++ acme_project/birthday/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.urls import reverse
+
+from .validators import real_age
+
+
+class Birthday(models.Model):
+    first_name = models.CharField(verbose_name='Имя', max_length=20)
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        blank=True,
+        help_text='Необязательное поле',
+        max_length=20
+    )
+    birthday = models.DateField(
+        verbose_name='Дата рождения',
+        validators=(real_age,)
+        )
+    image = models.ImageField(
+        verbose_name='Фото',
+        upload_to='birthdays_images',
+        default='birthdays_images/not_logo.jpg'
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint'
+            ),
+        )
+
+    def get_absolute_url(self):
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
